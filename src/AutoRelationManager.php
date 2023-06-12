7c5a15895ef592cb06b7b08cb879ced4cf1c7f71
miguilim <35383529+srdante@users.noreply.github.com>
2023-06-11 21:10:30 -0300

    Wip AutoRelationManager

--- src/AutoRelationManager.php
+++ src/AutoRelationManager.php
@@ -2,9 +2,33 @@
 
 namespace Miguilim\FilamentAutoResource;
 
+use Filament\Resources\Form;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
 
 class AutoRelationManager extends RelationManager
 {
+    public static array $visibleColumns = [];
+
+    public static bool $intrusive = true;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema(FilamentAutoResourceHelper::makeFormSchema(static::getRelatedModel()))
+            ->columns(3);
+    }
     
+    public static function tableExtra(Table $table): Table
+    {
+        return $table;
+    }
+
+    public static function table(Table $table): Table
+    {
+        $finalTable = static::tableExtra($table);
+
+        return $finalTable
+            ->columns(FilamentAutoResourceHelper::makeTableSchema(static::getRelatedModel(), static::$visibleColumns));
+    }
 }
---
+++ src/Commands/MakeAutoRelationManagerCommand.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Miguilim\FilamentAutoResource\Commands;
+
+use Filament\Commands\MakeRelationManagerCommand;
+
+class MakeAutoRelationManagerCommand extends MakeRelationManagerCommand
+{
+}
