60cd033e67d3490071d7ab9c8e6fd182ef9de158
miguilim <35383529+srdante@users.noreply.github.com>
2024-06-11 16:42:00 +0800

    Fix make:filament-auto-resource command

--- src/Commands/MakeAutoResourceCommand.php
+++ src/Commands/MakeAutoResourceCommand.php
@@ -14,7 +14,7 @@ class MakeAutoResourceCommand extends MakeResourceCommand
      *
      * @var string
      */
-    protected $signature = 'make:filament-auto-resource {name?} {--model-namespace=} {--panel=} {--F|force}';
+    protected $signature = 'make:filament-auto-resource {name?} {--model-namespace=} {--panel=} {--model} {--migration} {--factory} {--F|force}';
 
     /**
      * The console command description.
