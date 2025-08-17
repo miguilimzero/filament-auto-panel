51f2752a5e935e4d0cee2745803785ecbe590001
miguilimzero <35383529+miguilimzero@users.noreply.github.com>
2025-08-17 20:30:38 -0300

    Fix make:filament-auto-resource command

--- src/Commands/MakeAutoResourceCommand.php
+++ src/Commands/MakeAutoResourceCommand.php
@@ -2,53 +2,85 @@
 
 namespace Miguilim\FilamentAutoPanel\Commands;
 
-use ReflectionClass;
-use Illuminate\Support\Str;
+use Filament\Support\Commands\Concerns\CanManipulateFiles;
+use Filament\Support\Commands\Exceptions\FailureCommandOutput;
 
 use Filament\Commands\MakeResourceCommand;
 
 class MakeAutoResourceCommand extends MakeResourceCommand
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'make:filament-auto-resource {name?} {--model-namespace=} {--panel=} {--model} {--migration} {--factory} {--F|force}';
+    use CanManipulateFiles;
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Create a new Filament auto resource class';
 
-    public function option($key = null) { 
-        if ($key === 'generate') {
-            return true;
-        }
-        if ($key === 'soft-deletes' || $key === 'simple' || $key === 'view') {
-            return false;
-        }
+    protected $name = 'make:filament-auto-resource';
 
-        return parent::option($key);
-    }
-
-    protected function copyStubToApp(string $stub, string $targetPath, array $replacements = []): void 
-    { 
-        if($stub !== 'Resource') {
-            return;
-        }
-
-        parent::copyStubToApp('AutoResource', $targetPath, $replacements);
-    }
-
-    protected function getDefaultStubPath(): string
+    public function handle(): int
     {
-        $reflectionClass = new ReflectionClass($this);
+        try {
+            $this->configureModel();
+            $this->configureRecordTitleAttribute();
+            $this->configurePanel(question: 'Which panel would you like to create this resource in?');
+            // $this->configureIsSimple();
+            // $this->configureIsNested();
+            // $this->configureCluster();
+            $this->configureResourcesLocation(question: 'Which namespace would you like to create this resource in?');
+            // $this->configureParentResource();
+            // $this->configureHasViewOperation();
+            // $this->configureIsGenerated();
+            // $this->configureIsSoftDeletable();
+            $this->configureHasResourceClassesOutsideDirectories();
 
-        return (string) Str::of($reflectionClass->getFileName())
-            ->beforeLast('Commands')
-            ->append('../stubs');
+            $this->configureLocation();
+            // $this->configurePageRoutes();
+
+            // $this->createFormSchema();
+            // $this->createInfolistSchema();
+            // $this->createTable();
+
+            $this->createResourceClass();
+
+            // $this->createManagePage();
+            // $this->createListPage();
+            // $this->createCreatePage();
+            // $this->createEditPage();
+            // $this->createViewPage();
+        } catch (FailureCommandOutput) {
+            return static::FAILURE;
+        }
+
+        $this->components->info("Filament resource created successfully.");
+
+        if (empty($this->panel->getResourceNamespaces())) {
+            $this->components->info('Make sure to register the resource with [resources()] or discover it with [discoverResources()] in the panel service provider.');
+        }
+
+        return static::SUCCESS;
+    }
+
+    protected function createResourceClass(): void
+    {
+        $realFqnEnd = substr($this->fqnEnd, strpos($this->fqnEnd, '\\') + 1);
+
+        $path = (string) str("{$this->resourcesDirectory}\\{$realFqnEnd}.php")
+            ->replace('\\', '/')
+            ->replace('//', '/');
+
+        if (! $this->option('force') && $this->checkForCollision($path)) {
+            throw new FailureCommandOutput;
+        }
+
+        $recordTitleAttributeCode = '';
+        if ($this->recordTitleAttribute) {
+            $recordTitleAttributeCode = "\n\n    protected static string \$recordTitleAttribute = '{$this->recordTitleAttribute}';";
+        }
+
+        $this->copyStubToApp('AutoResource', $path, [
+            'namespace' => substr($this->namespace, 0, strrpos($this->namespace, '\\')),
+            'resourceClass' => $realFqnEnd,
+            'model' => $this->modelFqn,
+            'modelClass' => $this->modelFqnEnd,
+            'recordTitleAttributeCode' => $recordTitleAttributeCode,
+        ]);
     }
 }
